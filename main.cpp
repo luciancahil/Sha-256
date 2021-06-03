2b128da44b0eae1434ceba3a62699256188783cf
Roy He <royhe62@yahoo.ca>
2021-06-03 14:29:35 -0700

    added rotate functions

--- main.cpp
+++ main.cpp
@@ -50,6 +50,27 @@ class Sha256 {
 };
 
 
+int leftRotate(int n, unsigned int d)
+{
+
+    /* In n<<d, last d bits are 0. To
+     put first 3 bits of n at
+    last, do bitwise or of n<<d
+    with n >>(INT_BITS - d) */
+    return (n << d)|(n >> (INT_BITS - d));
+}
+
+/*Function to right rotate n by d bits*/
+int rightRotate(int n, unsigned int d)
+{
+    /* In n>>d, first d bits are 0.
+    To put last 3 bits of at
+    first, do bitwise or of n>>d
+    with n <<(INT_BITS - d) */
+    return (n >> d)|(n << (INT_BITS - d));
+}
+
+
 int main()
 {
     Sha256* hasher = new Sha256("Hello World");
